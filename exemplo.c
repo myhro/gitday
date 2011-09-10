226411651f471e5e2176ca1647a26aee52aea366
Tiago Ilieve <tiago.myhro@gmail.com>
2011-09-10 09:48:02 -0300

    gitignore adicionado

--- exemplo.c
+++ exemplo.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello World!");
+	printf("Hello World!\n");
+	while (1) {
+		printf("\nOpcoes: 1. Adicionar\n2. Remover\n3. Sair");
+		break;
+	}
 	return 0;
 }
